dae73fce4fcdb3bb6ab08e3d74bc3e1648fe4430
AyumiFurukawa <ayumi.furukawa.0616@gmail.com>
2023-06-20 18:38:43 +0900

    test

---
+++ react_todo/src/App.js
@@ -0,0 +1,62 @@
+import { useState, useRef } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+
+function App() {
+    const [todos, setTodos] = useState([]);
+
+    const todoNameRef = useRef();
+
+    const handleAddTodo = () => {
+        // タスクを追加する
+        const name = todoNameRef.current.value;
+        if (name === '') return;
+        setTodos((prevTodos) => {
+            return [
+                ...prevTodos,
+                { id: uuidv4(), name: name, completed: false },
+            ];
+        });
+        todoNameRef.current.value = null;
+    };
+
+    const toggleTodo = (id) => {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id === id);
+        todo.completed = !todo.completed;
+        setTodos(newTodos);
+    };
+
+    const handleClear = () => {
+        const newTodos = todos.filter((todo) => !todo.completed);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="container">
+            <div className="todo-container">
+                <label>
+                    <TodoList todos={todos} toggleTodo={toggleTodo} />
+                    <input
+                        type="text"
+                        ref={todoNameRef}
+                        className="todo-input"
+                    />
+                    <button onClick={handleAddTodo} className="add-todo">
+                        タスクを追加
+                    </button>
+                    <button onClick={handleClear} className="clear-completed">
+                        完了したタスクの削除
+                    </button>
+                    <div className="count-todo">
+                        残りのタスク:
+                        {todos.filter((todo) => !todo.completed).length}
+                    </div>
+                </label>
+            </div>
+        </div>
+    );
+}
+
+export default App;
